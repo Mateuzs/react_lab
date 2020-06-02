fc031d82c6994c9e29127bda07837e4ced82f68a
Mateuzs <mateusz.zembol@gmail.com>
2020-06-02 10:04:15 +0200

    added draggable components

--- src/components/DragContainer/DragContainer.tsx
+++ src/components/DragContainer/DragContainer.tsx
@@ -1,8 +1,20 @@
 import React, { FunctionComponent } from "react";
+import Draggable from "react-draggable";
+
 import "./DragContainer.scss";
 
 const DragContainer: FunctionComponent = () => (
-  <div className="drag-container"></div>
+  <div className="drag-container">
+    <Draggable bounds="parent">
+      <div className="drag-component__turquoise">Drag me please!</div>
+    </Draggable>
+    <Draggable bounds="parent">
+      <div className="drag-component__orange">Drag me please!</div>
+    </Draggable>
+    <Draggable bounds="parent">
+      <div className="drag-component__magenta">Drag me please!</div>
+    </Draggable>
+  </div>
 );
 
 export default DragContainer;
